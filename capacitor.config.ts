5f1d9eac851441fd161085974e92859b0f5ce703
LaurDaniel <laurentiuc111@gmail.com>
2022-04-04 15:51:46 +0300

    certificate

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ionicframework.MediCallPhone',
   appName: 'MediCall',
   webDir: 'www',
   bundledWebRuntime: false,
