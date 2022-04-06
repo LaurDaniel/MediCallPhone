b81766457fd93e4de0198cb4984f9f85b3f633e0
LaurDaniel <laurentiuc111@gmail.com>
2022-04-06 16:43:35 +0300

    record test

--- src/app/pages/customer/conferinta/conferinta.page.ts
+++ src/app/pages/customer/conferinta/conferinta.page.ts
@@ -18,17 +18,17 @@ declare var JitsiMeetExternalAPI: any;
   styleUrls: ['./conferinta.page.scss'],
  
 })
-export class ConferintaPage implements OnInit, AfterViewInit{
+export class ConferintaPage implements OnInit{
   domain: string = "voice.infragroup.ro"; // For self hosted use your domain
     room: any;
     options: any;
     api: any;
     user: any;
-    recording: boolean = false;
-filePath: string;
-fileName: string;
+//     recording: boolean = false;
+// filePath: string;
+// fileName: string;
 audio: MediaObject;
-audioList: any[] = [];
+// audioList: any[] = [];
     
   url = "https://probe.infragroup.ro";
   public url_conferinta: any;
@@ -40,47 +40,47 @@ audioList: any[] = [];
 
   constructor( private media: Media,private previewAnyFile: PreviewAnyFile, private platform: Platform, private file: File2,private menu: MenuController, private router: Router, private authService: AuthenticationService,private http: HttpClient) {
    }
-   getAudioList() {
-    if(localStorage.getItem("audiolist")) {
-      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
-      console.log(this.audioList);
-    }
-  }
-  ionViewWillEnter() {
-    this.getAudioList();
-  }
-  startRecord() {
-    if (this.platform.is('ios')) {
-      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
-      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
-      this.audio = this.media.create(this.filePath);
-    } else if (this.platform.is('android')) {
-      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
-      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
-      this.audio = this.media.create(this.filePath);
-    }
-    this.audio.startRecord();
-    this.recording = true;
-  }
-  stopRecord() {
-    this.audio.stopRecord();
-    let data = { filename: this.fileName };
-    this.audioList.push(data);
-    localStorage.setItem("audiolist", JSON.stringify(this.audioList));
-    this.recording = false;
-    this.getAudioList();
-  }
-  playAudio(file,idx) {
-    if (this.platform.is('ios')) {
-      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
-    } else if (this.platform.is('android')) {
-      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
-    }
-    this.audio.play();
-    this.audio.setVolume(0.8);
-  }
+  //  getAudioList() {
+  //   if(localStorage.getItem("audiolist")) {
+  //     this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+  //     console.log(this.audioList);
+  //   }
+  // }
+  // ionViewWillEnter() {
+  //   this.getAudioList();
+  // }
+  // startRecord() {
+  //   if (this.platform.is('ios')) {
+  //     this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+  //     this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+  //     this.audio = this.media.create(this.filePath);
+  //   } else if (this.platform.is('android')) {
+  //     this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+  //     this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+  //     this.audio = this.media.create(this.filePath);
+  //   }
+  //   this.audio.startRecord();
+  //   this.recording = true;
+  // }
+  // stopRecord() {
+  //   this.audio.stopRecord();
+  //   let data = { filename: this.fileName };
+  //   this.audioList.push(data);
+  //   localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+  //   this.recording = false;
+  //   this.getAudioList();
+  // }
+  // playAudio(file,idx) {
+  //   if (this.platform.is('ios')) {
+  //     this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+  //     this.audio = this.media.create(this.filePath);
+  //   } else if (this.platform.is('android')) {
+  //     this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+  //     this.audio = this.media.create(this.filePath);
+  //   }
+  //   this.audio.play();
+  //   this.audio.setVolume(0.8);
+  // }
  fetchUrl()
  {
   this.http.get(`${this.url}/api/conferinta/roomOpened/${localStorage.getItem("user_id")}`).subscribe(data=>{
@@ -101,6 +101,8 @@ audioList: any[] = [];
         this.fetchUrl();
     });
   }
+  else
+  this.audio.startRecord();
 
   }
 
@@ -152,24 +154,24 @@ this.http.get(`${this.url}/api/conferinta/filedownload/${nume_fisier}`)
   });
 }
   ////////////////meet
-  ngAfterViewInit(): void {
+//   ngAfterViewInit(): void {
 
  
-    this.options = {
-        roomName: '42',
-        width: 900,
-        height: 500,
-        jwt:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InplbGxpbSIsInN1YiI6InZvaWNlLmluZnJhZ3JvdXAucm8iLCJyb29tIjoiNDIiLCJjb250ZXh0Ijp7InVzZXIiOnsibmFtZSI6IlBhY2llbnQgVXNlcjEgVXNlcjEifX19.DVpTGjXm56RTFGOiGcN6mqSkb_5E097We5yUFvISmCI',
-        configOverwrite: { prejoinPageEnabled: false },
-        // interfaceConfigOverwrite: {
-        //     // overwrite interface properties
-        // },
-        parentNode: document.querySelector('#jitsi-iframe'),
+//     this.options = {
+//         roomName: '42',
+//         width: 900,
+//         height: 500,
+//         jwt:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJqaXRzaSIsImlzcyI6InplbGxpbSIsInN1YiI6InZvaWNlLmluZnJhZ3JvdXAucm8iLCJyb29tIjoiNDIiLCJjb250ZXh0Ijp7InVzZXIiOnsibmFtZSI6IlBhY2llbnQgVXNlcjEgVXNlcjEifX19.DVpTGjXm56RTFGOiGcN6mqSkb_5E097We5yUFvISmCI',
+//         configOverwrite: { prejoinPageEnabled: false },
+//         // interfaceConfigOverwrite: {
+//         //     // overwrite interface properties
+//         // },
+//         parentNode: document.querySelector('#jitsi-iframe'),
        
-    }
+//     }
 
-    this.api = new JitsiMeetExternalAPI(this.domain, this.options);
-}
+//     this.api = new JitsiMeetExternalAPI(this.domain, this.options);
+// }
  ///////////////
 
 }
