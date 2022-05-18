6b4dc06c56e01a242fc3f3e7eba6860fd243bd33
LaurDaniel <laurentiuc111@gmail.com>
2022-05-18 21:24:15 +0300

    useragnet

--- ios/App/App/public/cordova_plugins.js
+++ ios/App/App/public/cordova_plugins.js
@@ -10,14 +10,6 @@
         ]
         },
       {
-          "id": "cordova-plugin-advanced-http.http",
-          "file": "plugins/cordova-plugin-advanced-http/www/advanced-http.js",
-          "pluginId": "cordova-plugin-advanced-http",
-        "clobbers": [
-          "cordova.plugin.http"
-        ]
-        },
-      {
           "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
           "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
           "pluginId": "phonegap-plugin-barcodescanner",
@@ -34,43 +26,11 @@
         ]
         },
       {
-          "id": "cordova-plugin-datepicker.DatePicker",
-          "file": "plugins/cordova-plugin-datepicker/www/ios/DatePicker.js",
-          "pluginId": "cordova-plugin-datepicker",
-        "clobbers": [
-          "datePicker"
-        ]
-        },
-      {
           "id": "cordova-plugin-device.device",
           "file": "plugins/cordova-plugin-device/www/device.js",
           "pluginId": "cordova-plugin-device",
         "clobbers": [
           "device"
-        ]
-        },
-      {
-          "id": "im.ltdev.cordova.UserAgent.UserAgent",
-          "file": "plugins/im.ltdev.cordova.UserAgent/www/UserAgent.js",
-          "pluginId": "im.ltdev.cordova.UserAgent",
-        "clobbers": [
-          "UserAgent"
-        ]
-        },
-      {
-          "id": "cordova-plugin-media.Media",
-          "file": "plugins/cordova-plugin-media/www/Media.js",
-          "pluginId": "cordova-plugin-media",
-        "clobbers": [
-          "window.Media"
-        ]
-        },
-      {
-          "id": "cordova-plugin-media.MediaError",
-          "file": "plugins/cordova-plugin-media/www/MediaError.js",
-          "pluginId": "cordova-plugin-media",
-        "clobbers": [
-          "window.MediaError"
         ]
         },
       {
@@ -80,82 +40,13 @@
         "clobbers": [
           "window.PreviewAnyFile"
         ]
-        },
-      {
-          "id": "cordova-plugin-advanced-http.cookie-handler",
-          "file": "plugins/cordova-plugin-advanced-http/www/cookie-handler.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.dependency-validator",
-          "file": "plugins/cordova-plugin-advanced-http/www/dependency-validator.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.error-codes",
-          "file": "plugins/cordova-plugin-advanced-http/www/error-codes.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.global-configs",
-          "file": "plugins/cordova-plugin-advanced-http/www/global-configs.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.helpers",
-          "file": "plugins/cordova-plugin-advanced-http/www/helpers.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.js-util",
-          "file": "plugins/cordova-plugin-advanced-http/www/js-util.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.local-storage-store",
-          "file": "plugins/cordova-plugin-advanced-http/www/local-storage-store.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.lodash",
-          "file": "plugins/cordova-plugin-advanced-http/www/lodash.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.messages",
-          "file": "plugins/cordova-plugin-advanced-http/www/messages.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.ponyfills",
-          "file": "plugins/cordova-plugin-advanced-http/www/ponyfills.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.public-interface",
-          "file": "plugins/cordova-plugin-advanced-http/www/public-interface.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.tough-cookie",
-          "file": "plugins/cordova-plugin-advanced-http/www/umd-tough-cookie.js",
-          "pluginId": "cordova-plugin-advanced-http"
-        },
-      {
-          "id": "cordova-plugin-advanced-http.url-util",
-          "file": "plugins/cordova-plugin-advanced-http/www/url-util.js",
-          "pluginId": "cordova-plugin-advanced-http"
         }
     ];
     module.exports.metadata =
     // TOP OF METADATA
     {
-      "cordova-plugin-advanced-http": "3.3.1",
       "cordova-plugin-android-permissions": "1.1.3",
-      "cordova-plugin-datepicker": "0.9.3",
-      "cordova-plugin-media": "5.0.4",
       "cordova-plugin-preview-any-file": "0.2.9",
-      "im.ltdev.cordova.UserAgent": "1.0.1",
       "cordova-plugin-device": "2.1.0",
       "cordova-plugin-inappbrowser": "5.0.0",
       "phonegap-plugin-barcodescanner": "8.1.0"
